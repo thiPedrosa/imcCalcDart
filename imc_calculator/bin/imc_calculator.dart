1329541aea263a4d92ff5380ba9b7ca87b867703
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-10-12 17:59:47 -0300

    dart create app

---
+++ imc_calculator/bin/imc_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:imc_calculator/imc_calculator.dart' as imc_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imc_calculator.calculate()}!');
+}
---
+++ imc_calculator/test/imc_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:imc_calculator/imc_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
