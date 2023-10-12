edb9eea5a27fc3cd8c46caa96eec716a4fc8e26d
Thiago Pedrosa <thiagopedrosaw@gmail.com>
2023-10-12 18:10:46 -0300

    Create class Pessoa

---
+++ imc_calculator/lib/Classes/pessoa.dart
@@ -0,0 +1,31 @@
+class Pessoa {
+  String _nome = "";
+  double _altura = 0;
+  double _peso = 0;
+
+  void setNome(String nome) {
+    _nome = nome;
+  }
+
+  String getNome (String nome) {
+    return _nome;
+  }
+
+  void setAltura(double altura) {
+    _altura = altura;
+  }
+
+  double getAltura (double altura) {
+    return _altura;
+  }
+  
+  void setPeso(double peso) {
+    _peso = peso;
+  }
+
+  double getPeso (double peso) {
+    return _peso;
+  }
+
+
+}
--- imc_calculator/test/imc_calculator_test.dart
+++ imc_calculator/test/imc_calculator_test.dart
@@ -1,8 +1,7 @@
-import 'package:imc_calculator/imc_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(42, 42);
   });
 }
